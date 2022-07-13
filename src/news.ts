950d8362bc35d6656180e711e53e18db64797496
ShineiIchijo <johncena795001@gmail.com>
2022-07-13 16:04:04 +0530

    chore: setup project

---
+++ src/news.ts
@@ -0,0 +1,51 @@
+import cheerio from 'cheerio'
+import axios from 'axios'
+
+/**
+ * Scraps news from the given URL or ID of the news from MyAnimeList
+ * @param {number | string} query The URL or ID of the news
+ * @returns {Promise<INews>} The contents of the news page
+ * @example
+ * const query = 66853767 // 'https://myanimelist.net/news/66853767'
+ * scrapNews(query)
+ */
+export const scrapNews = async (query: string | number): Promise<INews> => {
+    if (!query) throw new TypeError(`'query' parameter shouldn't be undefined`)
+    const url =
+        typeof query === 'string' && query.includes('myanimelist.net/news/')
+            ? `https://myanimelist.net/news/${query.split('/')[query.split('/').length - 1]}`
+            : `https://myanimelist.net/news/${query}`
+    return await axios
+        .get(url)
+        .then(({ data }) => {
+            const $ = cheerio.load(data)
+            const title = $('#content > div.content-left > div > div:nth-child(2) > div > h1 > a').text()
+            const id = url.split('/')[url.split('/').length - 1]
+            const image =
+                $('#content > div.content-left > div > div:nth-child(2) > div > div.content.clearfix')
+                    .find('img')
+                    .attr('src') || 'https://i.imgur.com/Q8af0QU.jpg'
+            const description = $('#content > div.content-left > div > div:nth-child(2) > div > div.content.clearfix')
+                .text()
+                .replace('\n    \n', '')
+                .trim()
+            return {
+                id,
+                url,
+                image,
+                description,
+                title
+            }
+        })
+        .catch(() => {
+            throw new Error('Invalid news ID or URL for MyAnimeList')
+        })
+}
+
+export interface INews {
+    id: string
+    url: string
+    image: string
+    description: string
+    title: string
+}
